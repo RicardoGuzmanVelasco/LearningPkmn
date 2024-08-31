7e61c6300e771da895927127b6aa530f836e6471
RGV <ricardo.guzman.velasco@outlook.com>
2024-08-31 13:14:51 +0200

    feat: block movement for a while

--- Assets/Pkmn.Overworld/Runtime/Move.cs
+++ Assets/Pkmn.Overworld/Runtime/Move.cs
@@ -5,19 +5,31 @@ namespace Pkmn.Overworld.Runtime
 {
     public class Move : MonoBehaviour
     {
+        TimeSpan moveCooldown = TimeSpan.Zero;
+        
         void Update()
         {
-            if (UnityEngine.Input.GetKeyDown(KeyCode.UpArrow))
+            moveCooldown -= TimeSpan.FromSeconds(Time.deltaTime);
+            if (moveCooldown > TimeSpan.Zero)
+                return;
+            
+            if (UnityEngine.Input.GetKey(KeyCode.UpArrow))
                 MoveTo(Vector2Int.up);
-            if (UnityEngine.Input.GetKeyDown(KeyCode.DownArrow))
+            if (UnityEngine.Input.GetKey(KeyCode.DownArrow))
                 MoveTo(Vector2Int.down);
-            if (UnityEngine.Input.GetKeyDown(KeyCode.LeftArrow))
+            if (UnityEngine.Input.GetKey(KeyCode.LeftArrow))
                 MoveTo(Vector2Int.left);
-            if (UnityEngine.Input.GetKeyDown(KeyCode.RightArrow))
+            if (UnityEngine.Input.GetKey(KeyCode.RightArrow))
                 MoveTo(Vector2Int.right);
         }
 
         void MoveTo(Vector2Int towards)
+        {
+            moveCooldown = TimeSpan.FromSeconds(0.5);
+            TurnAndMoveTowards(towards);
+        }
+
+        void TurnAndMoveTowards(Vector2Int towards)
         {
             GetComponent<Turn>().LookTowards(towards);
             transform.position += new Vector3(towards.x, towards.y);
