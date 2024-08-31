0d3e09423cf263cfe74a35a03c7625103b8a9ed5
RGV <ricardo.guzman.velasco@outlook.com>
2024-08-31 12:11:03 +0200

    feat: being in the world

---
+++ Assets/Pkmn.Overworld/Runtime/Being.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Pkmn.Overworld.Runtime
+{
+    public class Being : MonoBehaviour
+    {
+        public Vector2Int Coords => EnsureCoords();
+
+        Vector2Int EnsureCoords()
+        {
+            var coords = transform.position;
+            var intCoords = new Vector2Int((int)coords.x, (int)coords.y);
+            if (coords.x != intCoords.x || coords.y != intCoords.y)
+                throw new NotSupportedException();
+
+            return intCoords;
+        }
+    }
+}
