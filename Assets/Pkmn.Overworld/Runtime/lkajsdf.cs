6437348c55f7a0a952a0f560b468172c123595b7
RGV <ricardo.guzman.velasco@outlook.com>
2024-09-01 20:15:18 +0200

    feat: set track when passing a tile

---
+++ Assets/Pkmn.Overworld/Runtime/lkajsdf.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace Pkmn.Overworld.Runtime
+{
+    internal class lkajsdf : MonoBehaviour
+    {
+        [SerializeField] string theme;
+        static string currentTheme;
+        
+        void Awake()
+        {
+            FindObjectOfType<Input>().GetComponent<Move>().JustMoved += ChangeThemeIfRedIsHere;
+        }
+
+        void ChangeThemeIfRedIsHere()
+        {
+            if(RedIsNotHere())
+                return;
+
+            if (theme == currentTheme)
+                return;
+                
+            Debug.Log("new song: " + theme);
+            currentTheme = theme;
+        }
+
+        bool RedIsNotHere()
+        {
+            return FindObjectOfType<Input>().GetComponent<Being>().Coords != GetComponent<Being>().Coords;
+        }
+    }
+}
