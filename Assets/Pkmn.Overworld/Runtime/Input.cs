14c16009e4700edcfd3d3cf5852b4e74da5ab694
RGV <ricardo.guzman.velasco@outlook.com>
2024-09-01 12:18:34 +0200

    refactor: move handling keys of movement to input

--- Assets/Pkmn.Overworld/Runtime/Input.cs
+++ Assets/Pkmn.Overworld/Runtime/Input.cs
@@ -6,12 +6,17 @@ namespace Pkmn.Overworld.Runtime
     {
         void Update()
         {
-           var direction = WhereIsLookingTowards();
-           if(direction is not null)
-               GetComponent<Turn>().LookTowards(direction.Value);
+            HandleTurn();
         }
-        
-        Vector2Int? WhereIsLookingTowards()
+
+        void HandleTurn()
+        {
+            var direction = WhereToLook();
+            if(direction is not null)
+                GetComponent<Turn>().LookTowards(direction.Value);
+        }
+
+        Vector2Int? WhereToLook()
         {
             if (UnityEngine.Input.GetKey(KeyCode.W))
                 return Vector2Int.up;
@@ -24,5 +29,20 @@ namespace Pkmn.Overworld.Runtime
 
             return null;
         }
+        
+        internal static Vector2Int? WhereToMove()
+        {
+            if (UnityEngine.Input.GetKey(KeyCode.UpArrow))
+                return Vector2Int.up;
+            if (UnityEngine.Input.GetKey(KeyCode.DownArrow))
+                return Vector2Int.down;
+            if (UnityEngine.Input.GetKey(KeyCode.LeftArrow))
+                return Vector2Int.left;
+            if (UnityEngine.Input.GetKey(KeyCode.RightArrow))
+                return Vector2Int.right;
+
+            return null;
+        }
+
     }
 }
--- Assets/Pkmn.Overworld/Runtime/Move.cs
+++ Assets/Pkmn.Overworld/Runtime/Move.cs
@@ -21,16 +21,7 @@ namespace Pkmn.Overworld.Runtime
 
         static Vector2Int? WhereToMove()
         {
-            if (UnityEngine.Input.GetKey(KeyCode.UpArrow))
-                return Vector2Int.up;
-            if (UnityEngine.Input.GetKey(KeyCode.DownArrow))
-                return Vector2Int.down;
-            if (UnityEngine.Input.GetKey(KeyCode.LeftArrow))
-                return Vector2Int.left;
-            if (UnityEngine.Input.GetKey(KeyCode.RightArrow))
-                return Vector2Int.right;
-
-            return null;
+            return Input.WhereToMove();
         }
 
         void BlockMovementAndMove(Vector2Int towards)
