0ec3611fb7be02a4bff18300b47574f55ab36c90
RGV <ricardo.guzman.velasco@outlook.com>
2024-09-01 19:31:14 +0200

    feat: repel fades with each step

--- Assets/Pkmn.Overworld/Runtime/Repel.cs
+++ Assets/Pkmn.Overworld/Runtime/Repel.cs
@@ -1,12 +1,28 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Pkmn.Overworld.Runtime
 {
     internal class Repel : MonoBehaviour
     {
+        int steps = 0;
+
+        void Awake()
+        {
+            FindObjectOfType<Input>().GetComponent<Move>().JustMoved += Step;
+        }
+
         public void Spray()
         {
             Debug.Log("Repel sprayed!");
+            steps = 5;
+        }
+
+        public void Step()
+        {
+            if (steps > 0)
+                steps--;
+            Debug.Log($"Repel steps left: {steps}");
         }
     }
 }
