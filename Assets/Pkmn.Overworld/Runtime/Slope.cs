8d6b38c80da6a52bc91e52d73494224272628543
RGV <ricardo.guzman.velasco@outlook.com>
2024-09-02 21:36:53 +0200

    feat: log if going up, down or sidewards in slopes

---
+++ Assets/Pkmn.Overworld/Runtime/Slope.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+namespace Pkmn.Overworld.Runtime
+{
+    internal class Slope : MonoBehaviour
+    {
+        Vector2Int DeDóndeVieneRed;
+        void Awake()
+        {
+            DeDóndeVieneRed = FindObjectOfType<Red>().WhereIs;
+            
+            FindObjectOfType<Red>().JustMoved += alsjkf;
+        }
+
+        void alsjkf()
+        {
+            if(RedIsNotHere())
+            {
+                DeDóndeVieneRed = FindObjectOfType<Red>().WhereIs;
+                return;
+            }
+            
+            if(DeDóndeVieneRed - FindObjectOfType<Red>().WhereIs == Vector2Int.down)
+                Debug.Log("Subir");
+            else if (DeDóndeVieneRed - FindObjectOfType<Red>().WhereIs == Vector2Int.up)
+                Debug.Log("Bajar");
+            else
+                Debug.Log("Vengo de lado");
+            DeDóndeVieneRed = FindObjectOfType<Red>().WhereIs;
+        }
+
+        bool RedIsNotHere()
+        {
+            return FindObjectOfType<Red>().WhereIs != GetComponent<IsInTheWorld>().Coords;
+        }
+    }
+}
