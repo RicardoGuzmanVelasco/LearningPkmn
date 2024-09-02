b5865abadde966aa87ed5c48374b48f9bfa0fa98
RGV <ricardo.guzman.velasco@outlook.com>
2024-09-02 21:54:03 +0200

    feat: log welcoming

--- Assets/Pkmn.Overworld/Runtime/Township.cs
+++ Assets/Pkmn.Overworld/Runtime/Township.cs
@@ -5,5 +5,28 @@ namespace Pkmn.Overworld.Runtime
     internal class Township : MonoBehaviour
     {
         [SerializeField] string mapArea;
+        static string currentMapArea = "";
+        
+        void Awake()
+        {
+            FindObjectOfType<Red>().JustMoved += ToastIfNewZone;
+        }
+
+        void ToastIfNewZone()
+        {
+            if (RedIsNotHere())
+                return;
+
+            if (currentMapArea == mapArea)
+                return;
+
+            Debug.Log("Welcome to " + mapArea);
+            currentMapArea = mapArea;
+        }
+        
+        bool RedIsNotHere()
+        {
+            return FindObjectOfType<Red>().WhereIs != GetComponent<IsInTheWorld>().Coords;
+        }
     }
 }
