61f7d240870afa2378bdd2d9cabdadc70e284119
RGV <ricardo.guzman.velasco@outlook.com>
2024-09-02 22:01:03 +0200

    fix: cross fade music

--- Assets/Pkmn.Overworld/Runtime/Music.cs
+++ Assets/Pkmn.Overworld/Runtime/Music.cs
@@ -9,7 +9,7 @@ namespace Pkmn.Overworld.Runtime
         bool firstIsPlaying = false;
         
         public bool IsPlaying(AudioClip clip)
-            => GetComponentsInChildren<AudioSource>().First().clip == clip || GetComponentsInChildren<AudioSource>().Last().clip == clip; 
+            => GetComponentsInChildren<AudioSource>()[firstIsPlaying ? 0 : 1].clip == clip; 
         
         public void Play(AudioClip theme)
         {
@@ -20,8 +20,8 @@ namespace Pkmn.Overworld.Runtime
                 ? GetComponentsInChildren<AudioSource>().Last()
                 : GetComponentsInChildren<AudioSource>().First();
             
-            sourceToFadeOut.DOFade(0, .25f);
-            sourceToFadeIn.DOFade(1, .25f);
+            sourceToFadeOut.DOFade(0, 1f);
+            sourceToFadeIn.DOFade(1, 1f);
             sourceToFadeIn.clip = theme;
             sourceToFadeIn.Play();
             
