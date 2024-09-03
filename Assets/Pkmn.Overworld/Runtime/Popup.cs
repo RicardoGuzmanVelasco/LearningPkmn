c240353b4c793c24d4bb5c01d7b1302cea72b513
RGV <ricardo.guzman.velasco@outlook.com>
2024-09-03 21:05:07 +0200

    feat: animate fade in and out

--- Assets/Pkmn.Overworld/Runtime/Popup.cs
+++ Assets/Pkmn.Overworld/Runtime/Popup.cs
@@ -1,12 +1,23 @@
-﻿using TMPro;
+﻿using System;
+using DG.Tweening;
+using TMPro;
 using UnityEngine;
 
 namespace Pkmn.Overworld.Runtime
 {
     internal class Popup : MonoBehaviour
     {
+        void Awake()
+        {
+            GetComponent<CanvasGroup>().alpha = 0;
+        }
+
         public void Say(string what)
         {
+            GetComponent<CanvasGroup>()
+                .DOFade(1, 1)
+                .SetEase(Ease.OutQuint)
+                .SetLoops(2, LoopType.Yoyo);
             GetComponentInChildren<TMP_Text>().text = what;
         }
     }
