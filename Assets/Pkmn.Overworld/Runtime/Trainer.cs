c547122440d8f0a0cac920bb60dfd51701783513
RGV <ricardo.guzman.velasco@outlook.com>
2024-09-12 20:24:44 +0200

    feat: trainer logs whats looking at

--- Assets/Pkmn.Overworld/Runtime/Trainer.cs
+++ Assets/Pkmn.Overworld/Runtime/Trainer.cs
@@ -1,9 +1,20 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Pkmn.Overworld.Runtime
 {
     public class Trainer : MonoBehaviour
     {
-        
+        void Awake()
+        {
+            var imin = GetComponent<IsInTheWorld>().Coords;
+            var imlookingat = GetComponent<Turn>().LookingTowards;
+            var infrontOf = FindObjectOfType<World>().EverythingAt(imin + imlookingat);
+            
+            foreach (var something in infrontOf)
+            {
+                Debug.Log(something);
+            }
+        }
     }
 }
