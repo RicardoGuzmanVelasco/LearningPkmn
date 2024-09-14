43f5f490319b30e1599d07adbc80eccc30cc3088
RGV <ricardo.guzman.velasco@outlook.com>
2024-09-14 19:49:07 +0200

    feat: log where youre trying to move to

--- Assets/Pkmn.Overworld/Runtime/TurnOrMoveInput.cs
+++ Assets/Pkmn.Overworld/Runtime/TurnOrMoveInput.cs
@@ -1,39 +1,58 @@
 ﻿using System;
 using UnityEngine;
+using static UnityEngine.Input;
+using static UnityEngine.KeyCode;
 
 namespace Pkmn.Overworld.Runtime
 {
     public class TurnOrMoveInput : MonoBehaviour
     {
+        Vector2Int whereIsTryingToMoveTo;
         internal void Handle()
         {
-            if (MustMove())
-            {
-                var direction = WhereToMove();
-                if (direction is not null)
-                    GetComponent<Move>().MoveTowardsIfIdle(direction.Value);
-            }
-            else
-                throw new NotImplementedException();
-        }
-
-        bool MustMove()
-        {
-            return true;
+            var direction = WhereToMove();
+            if (direction is not null)
+                GetComponent<Move>().MoveTowardsIfIdle(direction.Value);
         }
 
         static Vector2Int? WhereToMove()
         {
-            if (UnityEngine.Input.GetKey(KeyCode.UpArrow))
+            if (GetKey(UpArrow))
                 return Vector2Int.up;
-            if (UnityEngine.Input.GetKey(KeyCode.DownArrow))
+            if (GetKey(DownArrow))
                 return Vector2Int.down;
-            if (UnityEngine.Input.GetKey(KeyCode.LeftArrow))
+            if (GetKey(LeftArrow))
                 return Vector2Int.left;
-            if (UnityEngine.Input.GetKey(KeyCode.RightArrow))
+            if (GetKey(RightArrow))
                 return Vector2Int.right;
 
             return null;
+        }
+
+        void Update()
+        {
+            Debug.Log(whereIsTryingToMoveTo);
+
+            UpdateWhereIsTryingToMoveTo();
+            ResetIfJustReleased();
+        }
+
+        void ResetIfJustReleased()
+        {
+            if(GetKeyUp(UpArrow) || GetKeyUp(DownArrow) || GetKeyUp(LeftArrow) || GetKeyUp(RightArrow))
+                whereIsTryingToMoveTo = Vector2Int.zero;
+        }
+
+        void UpdateWhereIsTryingToMoveTo()
+        {
+            if (GetKey(UpArrow))
+                whereIsTryingToMoveTo = Vector2Int.up;
+            else if (GetKey(DownArrow))
+                whereIsTryingToMoveTo = Vector2Int.down;
+            else if (GetKey(LeftArrow))
+                whereIsTryingToMoveTo = Vector2Int.left;
+            else if (GetKey(RightArrow))
+                whereIsTryingToMoveTo = Vector2Int.right;
         }
     }
 }
