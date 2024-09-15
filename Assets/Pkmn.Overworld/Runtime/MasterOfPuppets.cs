2dcdc2b436e829147575076025bb014ce0c6c7cf
RGV <ricardo.guzman.velasco@outlook.com>
2024-09-15 17:37:52 +0200

    feat: muster of puppets disable free will

---
+++ Assets/Pkmn.Overworld/Runtime/MasterOfPuppets.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Pkmn.Overworld.Runtime
+{
+    public class MasterOfPuppets : MonoBehaviour
+    {
+        void Update()
+        {
+            if (UnityEngine.Input.GetKeyDown(KeyCode.K))
+                klasdf();
+        }
+
+        void klasdf()
+        {
+            foreach (var turnByFreeWill in FindObjectsOfType<TurnByFreeWill>())
+                turnByFreeWill.enabled = false;
+        }
+    }
+}
