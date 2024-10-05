b7a172cb066fa1bedd2ac9c35ab2457f787ef4c7
RGV <ricardo.guzman.velasco@outlook.com>
2024-10-05 16:38:33 +0200

    feat: log altruistic npc

---
+++ Assets/Pkmn.Overworld/Runtime/AltruistNpc.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Pkmn.Overworld.Runtime
+{
+    public class AltruistNpc : MonoBehaviour, IReply
+    {
+        public void Reply(Vector2Int towards)
+        {
+            Debug.Log("doy destello");
+        }
+
+        public Vector2Int Coords => GetComponent<IsInTheWorld>().Coords;
+    }
+}
