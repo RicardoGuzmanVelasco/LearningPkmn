9b3c4b45aa573d0b18f84994e815aeca335c1704
RGV <ricardo.guzman.velasco@outlook.com>
2024-11-09 16:56:29 +0100

    test: red cannot go down

---
+++ Assets/Pkmn.Overworld/Runtime/Tests/RegressionTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+namespace Pkmn.Overworld.Runtime.Tests
+{
+    public class RegressionTests
+    {
+        [UnityTest]
+        public IEnumerator RedCannotGoDown_InStartingPosition()
+        {
+            yield return SceneManager.LoadSceneAsync("Overworld", LoadSceneMode.Single);
+
+            var red = Object.FindObjectOfType<Red>();
+            var redPositionBefore = red.transform.position;
+            red.GetComponent<Move>().MoveTowardsIfIdle(Vector2Int.down);
+            
+            yield return new WaitForSeconds(1f);
+            
+            Assert.AreEqual(redPositionBefore, red.transform.position);
+        }
+    }
+}
