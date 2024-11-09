04fe1741711f8ef94081685e169a384b7b120945
RGV <ricardo.guzman.velasco@outlook.com>
2024-11-09 17:06:17 +0100

    test: property based test for walking

--- Assets/Pkmn.Overworld/Runtime/Tests/RegressionTests.cs
+++ Assets/Pkmn.Overworld/Runtime/Tests/RegressionTests.cs
@@ -36,5 +36,34 @@ namespace Pkmn.Overworld.Runtime.Tests
             
             Assert.That(red.transform.position.y, Is.GreaterThan(redPositionBefore.y));
         }
+
+        [UnityTest]
+        public IEnumerator Walk_ForAWhile()
+        {
+            yield return SceneManager.LoadSceneAsync("Overworld", LoadSceneMode.Single);
+
+            var red = Object.FindObjectOfType<Red>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var randomDirection = RandomDirection();
+                red.GetComponent<Move>().MoveTowardsIfIdle(randomDirection);
+                yield return new WaitForSeconds(.1f);
+            }
+            
+            Assert.Pass();
+        }
+
+        static Vector2Int RandomDirection()
+        {
+            var random = Random.Range(0, 4);
+            return random switch
+            {
+                0 => Vector2Int.up,
+                1 => Vector2Int.down,
+                2 => Vector2Int.left,
+                3 => Vector2Int.right,
+            };
+        }
     }
 }
