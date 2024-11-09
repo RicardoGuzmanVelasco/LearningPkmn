bbf4983c19bee7f357ffb60870ff35775b8f81eb
RGV <ricardo.guzman.velasco@outlook.com>
2024-11-09 16:43:38 +0100

    test: load scene

---
+++ Assets/Pkmn.Overworld/Runtime/Tests/ASDfasf.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine.Assertions;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+namespace Pkmn.Overworld.Runtime.Tests
+{
+    public class ASDfasf
+    {
+        [UnityTest]
+        public IEnumerator ASDfssssasDF()
+        {
+            yield return SceneManager.LoadSceneAsync("Overworld", LoadSceneMode.Single);
+        }
+    }
+}
