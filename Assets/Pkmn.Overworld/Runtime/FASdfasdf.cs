e3aac7b393ccfd28e1ef91f9a7643faec733ef3b
RGV <ricardo.guzman.velasco@outlook.com>
2024-11-09 18:01:17 +0100

    feat: SingleCombatShouldBeRaised_WhenThereAreTwoTrainers

--- Assets/Pkmn.Overworld/Runtime/FASdfasdf.cs
+++ Assets/Pkmn.Overworld/Runtime/FASdfasdf.cs
@@ -5,10 +5,15 @@ namespace Pkmn.Overworld.Runtime
 {
     public class FASdfasdf : MonoBehaviour
     {
+        bool challenged = false;
         public event Action IsImminent = () => { };
 
         public void ChallengeRed()
         {
+            if (challenged)
+                return;
+            challenged = true;
+            
             IsImminent();
         }
     }
